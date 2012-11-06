6b68a4f7873d9d5dac25ccdc31a4cfc1db6697d2
mrdizzy <david.pettifer@dizzy.co.uk>
2012-11-06 06:08:51 -0500

    Updated sortable

--- public/javascripts/couchdb_attachments.js
+++ public/javascripts/couchdb_attachments.js
@@ -1,55 +1,50 @@
    var Attachment = Backbone.Model.extend({
-        path: function(type) {
-            return this.collection.url + "/" + type + "-" + this.get("id")
-        }
-    });
-    
-    Backbone.View.Attachment = Backbone.View.extend({
-          initialize: function() {
-              var that = this;
-            this.$el.html("<div id=\"sortable\"></div>")
-            var sortableEl = this.$('#sortable');
-            sortableEl.sortable({update: function(a,b) {
-                console.log("Updatefing");
-                console.log(sortableEl.sortable("toArray"));
-                that.collection.belongs_to.set("attachments_order", sortableEl.sortable("toArray"));
-             }});
-          }
-    });
-    
-    var Attachments = Backbone.Collection.extend({
-        model: Attachment
-    });
-    Backbone.Model.CouchDB = Backbone.Model.extend({
-        initialize: function() {
-            console.log("Initializing");
-            this.attachments = new Attachments();
-            this.attachments.url = this.url() + "/attachments"
-            var model = this;
-            var attachments = _.map(this.get("attachments_order"), function(unique) {
-                
-                var attachment = {
-                    id: unique,
-                    _rev: model.get("_rev"),
-                }
-                return attachment;
-            }, this)
-            
-            this.attachments.reset(attachments);
-            this.attachments.belongs_to = this;
-            this.on('change', this.sortAttachments);
-        },
-        sortAttachments: function() {
-            console.log(this.get("attachments_order"));
-            var attachments = _.map(this.get("attachments_order"), function(unique) {
-                   var attachment = this.attachments.get(unique);
-                   
-            console.log("sorting attachments");
-                   console.log(attachment);
-                   attachment.set("_rev", this.get("_rev"));
-                   return attachment;
-            },this);
-            this.attachments.reset(attachments);
-        },
-        idAttribute: "_id"
-    });
+       path: function(type) {
+           return this.collection.url + "/" + type + "-" + this.get("id")
+       }
+   });
+
+   Backbone.View.Attachment = Backbone.View.extend({
+       initialize: function() {
+           var that = this;
+           this.$el.html("<div id=\"sortable\"></div>")
+           var sortableEl = this.$('#sortable');
+           sortableEl.sortable({
+               update: function(a, b) {
+                   that.collection.belongs_to.set("attachments_order", sortableEl.sortable("toArray"));
+               }
+           });
+       }
+   });
+
+   var Attachments = Backbone.Collection.extend({
+       model: Attachment
+   });
+   Backbone.Model.CouchDB = Backbone.Model.extend({
+       initialize: function() {
+           this.attachments = new Attachments();
+           this.attachments.url = this.url() + "/attachments"
+           var model = this;
+           var attachments = _.map(this.get("attachments_order"), function(unique) {
+               var attachment = {
+                   id: unique,
+                   _rev: model.get("_rev"),
+               }
+               return attachment;
+           }, this)
+
+           this.attachments.reset(attachments);
+           this.attachments.belongs_to = this;
+           this.on('change', this.sortAttachments);
+       },
+       sortAttachments: function() {
+           console.log(this.get("attachments_order"));
+           var attachments = _.map(this.get("attachments_order"), function(unique) {
+               var attachment = this.attachments.get(unique);
+               attachment.set("_rev", this.get("_rev"));
+               return attachment;
+           }, this);
+           this.attachments.reset(attachments);
+       },
+       idAttribute: "_id"
+   });
