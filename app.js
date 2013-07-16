50a34aa34fc36d9ee4d74d820d5b99599b96a93b
mrdizzy <david.pettifer@dizzy.co.uk>
2013-07-16 12:59:33 +0100

    Updatd

--- app.js
+++ app.js
@@ -86,8 +86,8 @@ app.get("/", function(req, res) {
     })
 });
 
-http.createServer(app).listen(process.env.PORT, function() {
-    console.log("Express server listening on port " + process.env.PORT);
+http.createServer(app).listen((process.env.PORT || 3000), function() {
+    console.log("Express server listening on port " + (process.env.PORT || 3000));
 });
 
 function parseRevision(req, id, callback) {
