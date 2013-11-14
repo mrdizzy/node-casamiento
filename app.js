702b28d65c4bc2f82df68aff3781ee48d481d1ae
mrdizzy <david.pettifer@dizzy.co.uk>
2013-11-14 16:08:04 +0000

    Updated

--- app.js
+++ app.js
@@ -50,11 +50,12 @@ var attachments = app.resource("attachments", require('./controllers/attachments
 
 products.add(attachments);
 
+// EBAY
 
 app.get('/ebayplaces', require('./controllers/ebay').places);
 var ebay = app.resource("ebay", require('./controllers/ebay'));
 
-ebay.get('/sample/:theme_id', require('./controllers/ebay').show);
+    ebay.get('/sample/:theme_id', require('./controllers/ebay').show);
 app.get('/ebay_single_places/:id', require('./controllers/ebay').name_place_single)
 
 // The eBay Notify API posts a message to this address that we need to parse
--- controllers/ebay.js
+++ controllers/ebay.js
@@ -1,20 +1,23 @@
 var db = require('./../config/db').test_ebay,
     _ = require('underscore');
 
+//casamiento.co.uk/ebay places -> exports.places -> renders all name place cards on one page
+// casamiento.co.uk/ebay/product-id -> exports.show -> renders the product id
+// casamiento.co.uk/ebay_single_places/product-id -> exports.name_place_single -> renders the product id
+
 // Route: /ebay/id
 // Renders the view found in catalog/
 exports.show = function(req, res) {
-console.log(req.params)
-if(req.params.ebay) {
+
+  if(req.params.ebay) {
     var theme = req.params.ebay.split("-")[0]
         var id = req.params.ebay;
     }
     else {
-            var theme = req.params.theme_id.split("-")[0]
- var id = req.params.theme_id;
- var sample = true;
+      var theme = req.params.theme_id.split("-")[0]
+      var id = req.params.theme_id;
+      var sample = true;
     }
-    console.log(sample)
     db.view('products/all', {
         startkey: theme,
         endkey: theme + "z"
@@ -30,7 +33,7 @@ if(req.params.ebay) {
         console.log(without);
         current.quantity = req.query.quantity || 1;
         current.auction = req.query.auction;
-        res.render('ebay/latest_16_sep_13.ejs', {
+        res.render('ebay/14_nov_13.ejs', {
             layout: false,
             locals: current
         });
@@ -38,29 +41,18 @@ if(req.params.ebay) {
 }
 
 exports.name_place_single = function(req, res) {
-console.log(req.params.id)
-        db.get(req.params.id, function(err, docs) {
-        console.log(err,docs)
-                res.render('ebay/name_place_single', {
-                        layout:false,
-                        locals: docs
-                })      
-        })     
+  db.get(req.params.id, function(err, docs) {
+    res.render('ebay/name_places/name_place_single', {
+            layout:false,
+            locals: docs
+    })      
+  })     
 }
 
 exports.places = function(req, res) {
        db.view('products/name_place', function(err, docs) {
        
-        res.render('ebay/name_places_old', {
-            layout: false,
-            documents: docs
-        });
-    });
-}
-exports.invitations = function(req, res) {
-       db.view('products/invitations', function(err, docs) {
-       
-        res.render('ebay/invitations', {
+        res.render('ebay/name_places/name_places_new_trial', {
             layout: false,
             documents: docs
         });
