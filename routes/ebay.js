1ccd5c3e3b88270e141c238cabaecab3ce3d9f86
unknown <david.pettifer@dizzy.co.uk>
2014-05-16 10:12:01 +0100

    Updated

--- routes/ebay.js
+++ routes/ebay.js
@@ -1,11 +1,11 @@
 module.exports = function(app){
 app.get("/ebay/places", require('./../controllers/ebay').places)
   app.get("/ebay", require('./../controllers/ebay').index);
-
+app.post("/ebay", require('./../controllers/ebay').create);
   app.get('/ebay/:id', require('./../controllers/ebay').show)
 
 // The eBay Notify API posts a message to this address that we need to parse
-app.post('/ebay', function(req, res) {
+app.post('/ebaymessage', function(req, res) {
     var result = "";
     req.on('data', function(chunk) {
         result += chunk;
