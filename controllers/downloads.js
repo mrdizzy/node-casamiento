b4d4b7dde5f1f60f1cd97375b2db2fd4d3d1744b
David <david.pettifer@dizzy.co.uk>
2014-05-21 14:29:55 +0100

    Upodated

--- controllers/downloads.js
+++ controllers/downloads.js
@@ -1,12 +1,10 @@
-var db = require('./../config/db').test_ebay,
-  _ = require('underscore');
+var db = require('./../config/db').test_ebay;
   
 exports.show = function(req, res) {
   var id = req.params.download;
- db.get(id, function(error, doc) {
-
-        res.render('downloads/show.ejs', {
-            locals: doc
-        })
-    });
+  db.get(id, function(error, doc) {
+    res.render('downloads/show.ejs', {
+        locals: doc
+    })
+  });
 };
