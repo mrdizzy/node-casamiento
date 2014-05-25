72ca4d7b59a895151e818fc0d5a26e556554e347
unknown <david.pettifer@dizzy.co.uk>
2014-05-25 21:33:06 +0100

    Updated£

--- controllers/svg.js
+++ controllers/svg.js
@@ -3,13 +3,38 @@ var db = require('./../config/db').test_ebay,
   
 
 exports.show = function(req, res) {
-  db.get("svg__" + req.params.id, function(error, results) {
-  console.log(req.params.id, error)
-    if(error) {
-      res.json({})
-    } else {
-      res.json(results)
-    }
-  })
+
+res.set("Content-Type", "text/plain")
+
+res.set("Content-Encoding", "gzip")
+
+  var stream = db.getAttachment("svg__damask-name_place", "gary2.svg.gz")
+  stream.on("data", function(chunk) {
+  console.log(chunk)
+    res.write(chunk)
+  });
+  stream.on('end', function() {
+
+    res.end();
+  });
+
+//    res.set("Content-Type", "image/svg+xml")
+//    res.set("Content-Encoding", "gzip")
+//  db.getAttachment("svg__damask-name_place", "damask.svgz", function (err, reply) {
+//  if (err) {
+//    console.log(err)
+//   
+//  }
+//}).pipe(res)
+//res.set(200)
+//res.end()
+// //db.get("svg__" + req.params.id, function(error, results) {
+ //console.log(req.params.id, error)
+ //  if(error) {
+ //    res.json({})
+ //  } else {
+ //    res.json(results)
+ //  }
+ //})
   
 }
