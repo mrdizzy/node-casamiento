a87d6a8ddc96a971f488063d51d27092716762f4
unknown <david.pettifer@dizzy.co.uk>
2014-07-27 19:14:39 +0100

    Added full fastclick as minified version not workign

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ var assetManagerGroups = {
         { 'route': /\/static\/javascripts\/all\.js/
         , 'path': __dirname + '/public/javascripts/libraries/'
         , 'dataType': 'javascript'
-        , 'files': [ 'fastclick.js', 'picturefill.min.js','in_groups_of.js', 'new_color_picker.js', 'font_picker.js','jquery_form.js' ]
+        , 'files': [ 'picturefill.min.js','in_groups_of.js', 'new_color_picker.js', 'font_picker.js','jquery_form.js' ]
     },
     'adminjs': 
         {
