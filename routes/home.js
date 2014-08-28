0449eefd4510246005bada5123c1113654eb1448
unknown <david.pettifer@dizzy.co.uk>
2014-08-28 12:24:37 +0100

    Udpated

--- routes/home.js
+++ routes/home.js
@@ -3,7 +3,8 @@ var _ = require('underscore'),
     
 module.exports = function(app){
   app.get("/", function(req, res) {
-    db.view('products/name_place', function(err, docs) {
+    db.get(["wild_vintage-name_place", "ace_of_hearts-name_place", "beautiful-name_place", "paisley-name_place", "damask-name_place", "rose-name_place", "e87cea260c13c66b90d544c55000292d-name_place", "birds_of_paradise-name_place", "chess-name_place", "borders-name_place", "e87cea260c13c66b90d544c550001945-name_place"], function(err, docs) {
+    console.log(docs.toArray())
       docs = docs.toArray();     
       docs.place_cards = docs;
       res.render('welcome/index', {
