94b725807df269d18ea37ecfc27ef776997a9a00
unknown <david.pettifer@dizzy.co.uk>
2014-09-09 21:50:10 +0100

    Updated

--- views/products/show/javascripts/ui_print_views.js
+++ views/products/show/javascripts/ui_print_views.js
@@ -98,7 +98,7 @@ var PlaceCardCollectionView = Backbone.View.extend({
     
     // Add crop marks
       var $container = $('<div class="up_' + options.per_page + '"></div>"');
-      if (options.per_page == 3) {
+      if ((options.per_page == 3) && options.svg) {
        $container.html('<div class="crop_marks"><div class="horizontal_crop_mark crop_top_bottom_left"></div><div class="horizontal_crop_mark crop_top_bottom_right"></div><div class="crop_top_top_left vertical_crop_mark"></div><div class="crop_top_top_right vertical_crop_mark"></div></div>')
        
        $container.append('<div class="crop_horizontal_left horizontal_crop_mark" style="top:52.125mm;"></div><div class="crop_horizontal_left horizontal_crop_mark" style="top:89.25mm;"></div><div class="crop_horizontal_left horizontal_crop_mark" style="top:126.375mm;"></div><div class="crop_horizontal_left horizontal_crop_mark" style="top:163.5mm;"></div><div class="crop_horizontal_left horizontal_crop_mark" style="top:200.625mm;"></div>')
@@ -107,7 +107,7 @@ var PlaceCardCollectionView = Backbone.View.extend({
        
        $container.append('<div class="crop_bottom_top_left horizontal_crop_mark"></div><div class="crop_bottom_top_right horizontal_crop_mark"></div><div class="crop_bottom_bottom_left vertical_crop_mark"></div><div class="crop_bottom_bottom_right vertical_crop_mark"></div>')
        }
-       else if (options.per_page == 4) {
+       else if ((options.per_page == 4) && options.svg) {
            $container.html('<div class="crop_marks"><div class="horizontal_crop_mark crop_top_bottom_left"></div><div class="horizontal_crop_mark crop_top_bottom_right"></div><div class="crop_top_top_left vertical_crop_mark"></div><div class="crop_top_top_right vertical_crop_mark"></div></div>')
        
        
@@ -115,6 +115,11 @@ var PlaceCardCollectionView = Backbone.View.extend({
        $container.append('<div class="crop_horizontal_right horizontal_crop_mark" style="top:52.125mm;"></div><div class="crop_horizontal_right horizontal_crop_mark" style="top:89.25mm;"></div><div class="crop_horizontal_right horizontal_crop_mark" style="top:126.375mm;"></div>')
        
        $container.append('<div class="crop_bottom_top_left horizontal_crop_mark"></div><div class="crop_bottom_top_right horizontal_crop_mark"></div><div class="crop_bottom_bottom_left vertical_crop_mark"></div><div class="crop_bottom_bottom_right vertical_crop_mark"></div>')
+       
+       } else if ((options.per_page == 8) && options.svg) {
+        $container.html('<div style="z-index:1001;width:10%;left:45%;height:12.5%;position:absolute;border-bottom:1px dashed lightgrey;"></div><div style="z-index:1001;width:10%;left:45%;height:25%;position:absolute;border-bottom:1px solid lightgrey;"></div><div style="z-index:1001;width:10%;left:45%;height:37.5%;position:absolute;border-bottom:1px dashed lightgrey;"></div><div style="z-index:1001;width:10%;left:45%;height:50%;position:absolute;border-bottom:1px solid lightgrey;"></div><div style="z-index:1001;width:10%;left:45%;height:62.5%;position:absolute;border-bottom:1px dashed lightgrey;"></div><div style="z-index:1001;width:10%;left:45%;height:75%;position:absolute;border-bottom:1px solid lightgrey;"></div><div style="z-index:1001;width:10%;left:45%;height:87.5%;position:absolute;border-bottom:1px dashed lightgrey;"></div>')
+          
+        $container.append('<div style="z-index:1001;height:2.5%;left:50%;position:absolute;border-left:1px solid lightgrey;"></div><div style="z-index:1001;height:5%;top:22.5%;left:50%;position:absolute;border-left:1px solid lightgrey;"></div><div style="z-index:1001;height:5%;top:47.5%;left:50%;position:absolute;border-left:1px solid lightgrey;"></div><div style="z-index:1001;height:5%;top:72.5%;left:50%;position:absolute;border-left:1px solid lightgrey;"></div><div style="z-index:1001;height:2.5%;bottom:0;left:50%;position:absolute;border-left:1px solid lightgrey;"></div>')
        }
        group.forEach(function(guest) {
         var place_card = new PlaceCardView(_.extend({
