659c3b6e9936b7d6c06539d90b3c09363956e022
unknown <david.pettifer@dizzy.co.uk>
2014-12-02 13:33:54 +0000

    updated

---
+++ views/ebay/name_places/css_partials/colour_picker.js
@@ -0,0 +1,76 @@
+
+ .dizzycp-static_container { 
+    background-color:white;
+  }
+  
+  .dizzycp-container_to_fade_in {
+    display:none; 
+    position:absolute; 
+    z-index:99999999999999999999999999999999999999999999999999999;
+    border:1px dotted black;
+  }
+  
+    .dizzycp-swatch_container { 
+      text-align:center;
+    }
+    
+      .dizzycp-big_colour_square_frame { 
+        cursor:pointer;
+        display:inline-block;
+        border:1px solid lightgrey;
+        width:40px;
+        height:40px;
+        padding:2px; 
+        border-radius:25px;
+      }
+        .dizzycp-big_colour_square_swatch {
+          width:36px; 
+          height:36px;
+          margin:2px; 
+          border-radius:20px;
+        }
+      .dizzycp-text_label_for_colour { 
+        color:black;
+        padding-top:0.25em;
+        padding-bottom:0.25em;
+        font:normal 1.2em 'OS';
+        text-transform:uppercase;
+        letter-spacing:0.05em;
+      }
+
+.dizzycp-external_wrapper {
+  margin:0 auto;
+  overflow:hidden;
+}
+.dizzycp-left_arrow, .dizzycp-right_arrow {
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  -khtml-user-select: none;
+  -moz-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
+  cursor: pointer;
+}
+.dizzycp-left_arrow { 
+  margin-left:50px; 
+  float:left;
+  font-size:50px;
+}
+.dizzycp-right_arrow { 
+  margin-right:50px; 
+  float:right;
+  font-size:50px;
+}
+.dizzycp-internal_wrapper{
+  overflow-y: hidden;
+  overflow-x: scroll;    
+  -webkit-overflow-scrolling: touch;
+}
+.square { 
+  float:left;
+  cursor: pointer;
+}
+.dizzycp-scrollable_colours {
+  height:100px;
+  position:relative;
+}
