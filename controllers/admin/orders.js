8830ddcc136ed853e4988ce14d9f386e0d99a3c0
unknown <david.pettifer@dizzy.co.uk>
2015-01-27 15:35:42 +0000

    Updated

---
+++ controllers/admin/orders.js
@@ -0,0 +1,18 @@
+var db = require('./../../config/db').test_ebay;
+
+exports.index = function(req, res) {
+  db.view('all/type', {
+    key: 'order'
+  }, function(error, docs) {
+    if (error) {
+      console.log(error)
+    }
+    else {
+      res.render("admin/orders/index", {
+        orders: docs.toArray(), 
+        layout:"admin_layout"
+      })
+     
+    }
+  })
+}
