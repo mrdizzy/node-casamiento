f191d2119368093e0cb2abe1d928df3a0c00e850
mrdizzy <david.pettifer@dizzy.co.uk>
2015-07-11 10:52:41 +0100

    Updated

--- config/paypal_config.js
+++ config/paypal_config.js
@@ -20,9 +20,9 @@ var PayPal = require('node-paypal-api-nv'),
     // Pass true as the final parameter to new PayPal() if we are using the sandbox
   module.exports = function() {
    // if (process.env.NODE_ENV == 'test') {
-        return new PayPal(sandbox.USER, sandbox.PWD, sandbox.SIGNATURE, true);
+       // return new PayPal(sandbox.USER, sandbox.PWD, sandbox.SIGNATURE, true);
    // }
    // else {
-  //      return new PayPal(production.USER, production.PWD, production.SIGNATURE, false);
+        return new PayPal(production.USER, production.PWD, production.SIGNATURE, false);
    // }
 }
