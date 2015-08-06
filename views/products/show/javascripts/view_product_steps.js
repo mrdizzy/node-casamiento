e249512eb5af9ead4fe0df42f6511e76f426f3db
unknown <david.pettifer@dizzy.co.uk>
2015-08-06 15:50:28 +0100

    updated

--- views/products/show/javascripts/view_product_steps.js
+++ views/products/show/javascripts/view_product_steps.js
@@ -1,17 +1,3 @@
-function setSelectionRange(input, selectionStart, selectionEnd) {
-  if (input.setSelectionRange) {
-    input.focus();
-    input.setSelectionRange(selectionStart, selectionEnd);
-  }
-  else if (input.createTextRange) {
-    var range = input.createTextRange();
-    range.collapse(true);
-    range.moveEnd('character', selectionEnd);
-    range.moveStart('character', selectionStart);
-    range.select();
-  }
-}
-
 // STEP VIEW
 ////////////////////////////////////////////////////////////////////////////// 
 var StepView = BackboneRelativeView.extend({ 
