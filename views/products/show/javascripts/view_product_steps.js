4691abbfc1bc3b874f92418b174bf2848d4bd34b
unknown <david.pettifer@dizzy.co.uk>
2015-08-11 09:55:27 +0100

    Updated

--- views/products/show/javascripts/view_product_steps.js
+++ views/products/show/javascripts/view_product_steps.js
@@ -29,8 +29,7 @@ var StepView = BackboneRelativeView.extend({
   // the layout for mobile devices that do not have enough screen space
   selectQuickGuests: function() { 
     $('body').addClass("quick_guests_selected");
-    //window.scrollTo(0,0)
-    //app_router.navigate("editing_quickguests")
+    app_router.navigate("editing_quickguests")
   },
   hideQuickGuests: function() { $('body').removeClass("quick_guests_selected") },
   // If you happen to click somewhere else on the textarea in the split second before rendering, this stops the caret from jumping to the previous location
