e8a4562751a0318f7e2612157d2c509f101a13cc
mrdizzy <david.pettifer@dizzy.co.uk>
2015-09-17 00:16:29 +0000

    Updated

--- controllers/about.js
+++
@@ -1,9 +0,0 @@
-var _ = require('underscore'),
-		db = require('./../config/db').test_ebay;
-
-exports.index = function(req, res) {
-
-		res.render('welcome/about', {
-			layout:"about_layout",
-		});
-}
